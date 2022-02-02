19adc216af962e58fc99c3f6f63cf976d4c46789
shantam8 <dscharmen@aol.com>
2022-02-02 18:31:38 +0100

    tech page done all sizes

--- js/main.js
+++ js/main.js
@@ -4,9 +4,11 @@ let navbar = document.querySelector(".nav__content");
 let btnsDestinationSelect = document.querySelectorAll(
   ".text-content__destination-buttons button"
 );
-
 let btnsCrewSelect = document.querySelectorAll(
   ".text-content__crew-buttons button"
+);
+let btnsTechnologySelect = document.querySelectorAll(
+  ".text-content__technology-buttons button"
 );
 
 let data;
@@ -31,7 +33,22 @@ function handleDestinationSelection(event) {
   }
 }
 
-function handleCrewSelection(event){
+function fillDestinationCard(cardData) {
+  let name = cardData.name;
+  let description = cardData.description;
+  let distance = cardData.distance;
+  let travel = cardData.travel;
+  let image = cardData.images.png;
+  image = "." + image;
+
+  document.querySelector("#destination__img").src = image;
+  document.querySelector("#destination__title").textContent = name;
+  document.querySelector("#destination__description").textContent = description;
+  document.querySelector("#destination__distance").textContent = distance;
+  document.querySelector("#destination__travel").textContent = travel;
+}
+
+function handleCrewSelection(event) {
   switch (event.target.value) {
     case "0":
       fillCrewCard(data.crew[0]);
@@ -48,35 +65,44 @@ function handleCrewSelection(event){
   }
 }
 
-function fillDestinationCard(cardData) {
-  let name = cardData.name;
-  let description = cardData.description;
-  let distance = cardData.distance;
-  let travel = cardData.travel;
-  let image = cardData.images.png;
-  image = "." + image
-  
-  document.querySelector("#destination__img").src = image;
-  document.querySelector("#destination__title").textContent = name;
-  document.querySelector("#destination__description").textContent = description;
-  document.querySelector("#destination__distance").textContent = distance;
-  document.querySelector("#destination__travel").textContent = travel;
-}
-
 function fillCrewCard(cardData) {
-  console.log("asd")
+  console.log("asd");
   let image = cardData.images.png;
   let rank = cardData.role;
   let name = cardData.name;
   let description = cardData.bio;
-  image = "." + image
-  
+  image = "." + image;
+
   document.querySelector("#crew__img").src = image;
-  document.querySelector("#crew__img").setAttribute("aria-label", "image " +rank);
+  document
+    .querySelector("#crew__img")
+    .setAttribute("aria-label", "image " + rank);
   document.querySelector("#crew__img").alt = rank;
   document.querySelector("#crew__rank").textContent = rank;
   document.querySelector("#crew__name").textContent = name;
   document.querySelector("#crew__description").textContent = description;
+}
+
+function handleTechnologySelection(event) {
+  switch (event.target.textContent) {
+    case "1":
+      fillTechnologyCard(data.technology[0]);
+      break;
+    case "2":
+      fillTechnologyCard(data.technology[1]);
+      break;
+    case "3":
+      fillTechnologyCard(data.technology[2]);
+      break;
+  }
+}
+
+function fillTechnologyCard(cardData) {
+  let name = cardData.name;
+  let description = cardData.description;
+
+  document.querySelector("#technology__title").textContent = name;
+  document.querySelector("#technology__description").textContent = description;
 }
 
 function fetchData() {
@@ -96,6 +122,10 @@ function init() {
 
   btnsCrewSelect.forEach((button) => {
     button.addEventListener("click", handleCrewSelection);
+  });
+
+  btnsTechnologySelect.forEach((button) => {
+    button.addEventListener("click", handleTechnologySelection);
   });
   fetchData();
 }
